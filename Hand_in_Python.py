a504d3c5fcd635d1ca35edf64f9d3fb5aa8634e6
Vivian van Oosten <fif@amvi.nl>
2020-01-14 14:47:48 +0100

    Dictionary with totals per month & complete total
    Summed per month and total. Made a file out of that.

--- Hand_in_Python.py
+++ Hand_in_Python.py
@@ -2,20 +2,34 @@ import json
 from datetime import datetime
 from datetime import date
 
+#Loading the precipitation file
 with open('precipitation.json', encoding='utf8') as file:
     rain = json.load(file)
 
+#initialising a list of Seattle-observations
 seattle_observations = []
 
+#Changing the 'date' value into a datetime object to be able to access the months
 for rain_observation in rain:
     if rain_observation['station'] == 'GHCND:US1WAKG0038':
         rain_datetime = datetime.strptime(rain_observation['date'], '%Y-%m-%d')
         rain_observation['date'] = rain_datetime.date()
         seattle_observations.append(rain_observation)
 
+seattle_rain_per_month = {}
 
+for month in range(1,13):
+    seattle_rain_per_month[month] = 0
+    for seattle_rain_observation in seattle_observations:
+        if seattle_rain_observation['date'].month == month:
+            seattle_rain_per_month[month] += seattle_rain_observation['value']
 
+seattle_total_rain = sum(seattle_rain_per_month.values())
 
+seattle_rain_per_month['Total'] = seattle_total_rain
+
+with open('Exercise1.json', 'w', encoding='utf8') as file:
+    json.dump(seattle_rain_per_month, file)
 
 # with open('file_name.json', 'w', encoding='utf8') as file:
 #     json.dump(some_data, file)
